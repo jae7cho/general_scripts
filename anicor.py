1ffe9b1bbcd082b6feb9504a784bc40ef33dd5a5
Jae Wook Cho <jae7cho@gmail.com>
2019-08-13 12:33:17 -0400

    Add files via upload

---
+++ anicor.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 12 10:13:21 2018
+
+@author: Anibal Solon
+"""
+
+import numpy as np
+
+
+def zscore(data, axis):
+    data -= data.mean(axis=axis, keepdims=True)
+    data /= data.std(axis=axis, keepdims=True)
+    return np.nan_to_num(data, copy=False)
+
+
+def correlation(matrix1, matrix2):
+    d1 = matrix1.shape[-1]
+    d2 = matrix2.shape[-1]
+
+    assert d1 == d2
+    assert matrix1.ndim <= 2
+    assert matrix2.ndim <= 2
+    
+    matrix1 = zscore(matrix1.astype(float), matrix1.ndim - 1) / np.sqrt(d1)
+    matrix2 = zscore(matrix2.astype(float), matrix2.ndim - 1) / np.sqrt(d2)
+    
+    if matrix1.ndim >= matrix2.ndim:
+        return np.dot(matrix1, matrix2.T)
+    else:
+        return np.dot(matrix2, matrix1.T)
+
